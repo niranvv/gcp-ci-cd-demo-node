f402bca911a3bdf118f1e8f22590910e43d4e7ad
Niran Vijayakumar <niranvv@gmail.com>
2023-10-13 20:39:01 +0530

    Update app.js for testing cloud run & app engine deployments

--- node-hello-world/app.js
+++ node-hello-world/app.js
@@ -5,7 +5,7 @@ var app = express();
 
 
 app.get('/', function (req, res) {
-  res.send('Hello DevOps Malayalam - from Node (v2)!<br/>Folder:node-hello-world from Hostname:' + os.hostname());
+  res.send('Hello DevOps Malayalam - from Node (v1)!<br/>Folder:node-hello-world from Hostname:' + os.hostname());
 });
 
 const PORT = parseInt(process.env.PORT) || 8080;
